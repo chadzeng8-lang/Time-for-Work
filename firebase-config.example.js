c6305875a232de0a295a71a15ae655a7fd1da9dd
chadzeng8-lang <chadzeng8@gmail.com>
2026-07-09 10:17:52 +0800

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-// 复制此文件为 firebase-config.js，并填入你在 Firebase 控制台拿到的配置
-window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
